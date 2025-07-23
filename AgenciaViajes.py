f47e3acf6a70bc21902ea0488324f96cd62e33c8
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-07-23 11:01:59 -0600

    se agrego el comienzo de la funcion para saber cual cliente es el que tiene mas destinos ademas de comenzar a poder ingresar quien es el cliente con mas destinos

--- AgenciaViajes.py
+++ AgenciaViajes.py
@@ -6,6 +6,10 @@ def cantidad_destinos(cantidad, cantidad_clientes, cantidad_viajes):
         return cantidad_viajes
 
 
+def cliente_destinos_mas(clientes, lista_clientes, indice=0, cliente_mayor='', maximos_destinos=0):
+    if indice >= len(lista_clientes):
+        return cliente_mayor, maximos_destinos
+
 
 clientes = {}
 cantidad_viajes = 0
@@ -15,7 +19,12 @@ while opcion != 3:
     print("1. Registrar clientes")
     print("2. Mostrar lista de clientes")
     print("3. salir")
-    opcion = int(input("Selecciona una opcion: "))
+    try:
+        opcion = int(input("Selecciona una opcion: "))
+    except ValueError:
+        print("Opcion no valida")
+        continue
+
     if opcion == 1:
         cantidad_viajes = 0
         print("=== Registrar clientes ===")
@@ -32,6 +41,7 @@ while opcion != 3:
             }
 
             numero_destinos = int(input("¿Cuántos destinos desea registrar?: "))
+            cantidad_destino = numero_destinos
             for b in range(numero_destinos):
                 nombre_destino = input(f"Destino {b + 1}: ")
                 clientes[carnet]['destinos'][nombre_destino] = {}
@@ -50,3 +60,7 @@ while opcion != 3:
         print("=== DESTINOS VISITADOS ===")
         print(f"Total de destinos registrados entre todos los clientes: {cantidad_viajes}")
 
+        lista_clientes = list(clientes.keys())
+        if lista_clientes:
+            cliente_mayor, total_destinos = cliente_destinos_mas(clientes, lista_clientes)
+            print(f"Cliente con más destinos: {clientes[cliente_mayor]['nombre']} ({total_destinos} visitados)")
